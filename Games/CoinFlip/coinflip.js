b6bf3441fb8f5d861441da8783ad8b32483cc770
Leia Quesada <leiaquesada@gmail.com>
2021-02-09 10:27:07 -0800

    Add a screen to window

--- Games/CoinFlip/coinflip.js
+++ Games/CoinFlip/coinflip.js
@@ -7,13 +7,15 @@ Extensions:
 Add a box where the user can say how many coins they want to flip. Then display that many coins and flip them all randomly.
 */
 
-function coinFlip() {
-    // ask user how many coins they want to flip
-    // how do you turn this into a box?
-    let flipTimes = prompt("How many coins would you like to flip?");
-    // return heads or tails 50% of times
-    let heads = flipTimes/2; 
-    let tails = flipTimes/2;
-    // display flipTimes many coins and flip them all randomly
-    
-}
+let canvas = document.getElementById("gameScreen");
+//
+//function coinFlip() {
+//    // ask user how many coins they want to flip
+//    // how do you turn this into a box?
+//    let flipTimes = prompt("How many coins would you like to flip?");
+//    // return heads or tails 50% of times
+//    let heads = flipTimes/2; 
+//    let tails = flipTimes/2;
+//    // display flipTimes many coins and flip them all randomly
+//    
+//}
