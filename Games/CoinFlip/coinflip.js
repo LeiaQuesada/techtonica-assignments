321589cc82de1040564504595001c4c363c5cc84
Leia Quesada <leiaquesada@gmail.com>
2021-02-08 22:52:58 -0800

    Coin Flip game first commit

---
+++ Games/CoinFlip/coinflip.js
@@ -0,0 +1,19 @@
+/*
+Coin Flip
+Make a web page where you can flip a coin and it comes up heads 50% of times and tails 50% of times.
+
+Extensions:
+
+Add a box where the user can say how many coins they want to flip. Then display that many coins and flip them all randomly.
+*/
+
+function coinFlip() {
+    // ask user how many coins they want to flip
+    // how do you turn this into a box?
+    let flipTimes = prompt("How many coins would you like to flip?");
+    // return heads or tails 50% of times
+    let heads = flipTimes/2; 
+    let tails = flipTimes/2;
+    // display flipTimes many coins and flip them all randomly
+    
+}
