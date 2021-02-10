40d0dc4dd1f5f031f78e808762faea4ca3e76195
Leia Quesada <leiaquesada@gmail.com>
2021-02-10 14:03:35 -0800

    add dynamic function to webpage

--- OOP/OOPProject/event.js
+++ OOP/OOPProject/event.js
@@ -28,3 +28,34 @@ const eventObj1 = new Event(
   'KLOS Golden Gala',
   'An evening with hollywood vampires'
 );
+
+// instantiate new objects
+const eventObj2 = new Event('Skillet & Sevendust', 'Victorious war tour');
+const eventObj3 = new Event('Jenny Lewis', 'On the line tour 2019');
+
+// creates an empty Event array
+const eventArray = new Array();
+
+// pushes single object to an array
+eventArray.push(eventObj1);
+
+// pushes multiple objects to an array at once
+eventArray.push(eventObj1, eventObj2, eventObj3);
+
+// in order to check whether the elements are pushed, use console.log
+console.log(eventArray);
+
+//  jQuery code to iterate through it in the same js file
+// .each() is used to iterate through the array of objects. Above code represents the syntax to iterate using .each()
+// .html() is used to return the HTML code from jQuery to the target element of the HTML page.
+// event is the target element in the below code.
+// Note that all jQuery methods in our examples are inside a document ready event. This is to prevent any jQuery code from running before the document is finished loading (is ready).
+$(document).ready(function () {
+  let html = '';
+  $.each(eventArray, function (index, item) {
+    html += `<li>${item.name} - ${item.description}</li>`;
+  });
+  // insert final html into #event...
+  $('#event').html(html);
+});
+
