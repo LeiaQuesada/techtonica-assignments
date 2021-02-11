a2d690d7f94967e7b4bb26b085fbf75336fe0ff0
Leia Quesada <leiaquesada@gmail.com>
2021-02-10 20:08:20 -0800

    Add all tickets method on event class and improve addAvail method

--- OOP/OOPProject/event.js
+++ OOP/OOPProject/event.js
@@ -21,13 +21,30 @@ class Event {
       this.description = description;
       this.availableTickets = [];
     }
+    // add function that will create ticket type for the event taking 2 params, name of the ticket type and the price of the ticket
     addAvailableTickets(type, price) {
-      // use Event's propert to store ticket types for the event
+      // use Event's property to store ticket types for the event
       this.type = type;
       this.price = price;
+      let types = {};
+      types[this.type] = this.price;
+      this.availableTickets.push(types);
+      return this.availableTickets;
+    }
+    allTickets() {
+      this.addAvailableTickets.types;
+      let str = "All tickets: ";
+      let count = 1;
+      for (let element in types) {
+        str += `${count}. ${types[element][this.type]} (${types[this.price]}) `;
+        count++;
+      }
+      console.log(str);
+      return str;
     }
 }
 
+// create a class to give events ability to have different ticket types with different prices
 class TicketType {
     constructor(name, price) {
       this.name = name;
@@ -54,13 +71,27 @@ const eventArray = new Array();
 eventArray.push(eventObj1, eventObj2, eventObj3);
 
 // in order to check whether the elements are pushed, use console.log
-console.log(eventArray);
+// console.log(eventArray);
+
+// add different types to every single one of your events. Here are some more examples for how it will be called
+eventObj1.addAvailableTickets("human", 299);
+eventObj1.addAvailableTickets("vampire", 99);
+
+eventObj2.addAvailableTickets("General Admission", 25);
+eventObj2.addAvailableTickets("Floor Seating", 80);
+
+eventObj3.addAvailableTickets("Orchestra", 300);
+eventObj3.addAvailableTickets("Mezzanine", 200);
+eventObj3.addAvailableTickets("Balcony", 100);
+
+console.log(eventObj1.availableTickets);
 
 //  jQuery code to iterate through it in the same js file
-// .html() is used to return the HTML code from jQuery to the target element of the HTML page. event is the target element in the below code.
 // Note that all jQuery methods in our examples are inside a document ready event. This is to prevent any jQuery code from running before the document is finished loading (is ready).
 $(document).ready(function () {
+  // .html() is used to return the HTML code from jQuery to the target element of the HTML page. event is the target element in the below code.
   let html = '';
+  // .each() is used to iterate through the array of objects.
   $.each(eventArray, function (index, item) {
     html += `<li>${item.name} - ${item.description}</li>`;
   });
