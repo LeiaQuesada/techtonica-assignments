3c14dfcfa4fc648850097b6a308edd0d0ce9a04b
Leia Quesada <leiaquesada@gmail.com>
2021-02-12 00:37:24 -0800

    Pseudocode it out

--- Games/CoinFlip/static/coinflip.js
+++ Games/CoinFlip/static/coinflip.js
@@ -7,27 +7,52 @@ Extensions:
 Add a box where the user can say how many coins they want to flip. Then display that many coins and flip them all randomly.
 */
 
-const coinFlips = document.getElementById("flipCount");
+const coinFlips = document.getElementById("numberChosen");
 const image = document.getElementsByTagName("a");
 const userResultAlert = document.getElementById("userAlert");
 const display = document.getElementsByClassName("display");
 
 
 document.getElementById("game-form").onsubmit = () => {
-// display heads or tails consecutively after each call to flip, add coins raining in the background later
+    // number of chances user wants
+    let flips = coinFlips.value;
+    // random result of heads
+    let heads = 0; // heads will be 1 for randomNum
+    let tails = 0; // tails will be 2 for randomNum
 
-// if heads results are greater than tails, congratulate player
+    // display heads or tails consecutively after each call to flip, add coins raining in the background later
+    for (let i = 0; i <= flips; i++) {
+        let randomNum = Math.round(Math.random() + 1);
+        if (randomNum === 1) {
+            // display heads coin
 
-// play video of cute cat with big eyes about to jump at screen, cute cat purring, some loving image with a cat
+            heads++
+        } else
+        if (randomNum === 2) {
+            // display tails coin
+            tails++;
+        }
+    }
+    // if heads result === tails result 
+    if (heads === tails) {
+        // restart? undecided? milton meh?
 
-// if tails results are greater, tell player to duck, or run
+    // if heads results are greater than tails
+    } else
+    if (heads > tails) {
+        // congratulate player
 
-//play video of cat with claws out leaping at screen, display cat with stank attitude, nose turned up, claws out, sticking tongue out, 
+    
+        // play video of cute cat with big eyes about to jump at screen, cute cat purring, some loving image with a cat
 
-// if heads result === tails result return flip again
+    // if tails results are greater
+    } else {
+        // tell player to duck, or run
 
-// create a reset game button. 
+        //play video of cat with claws out leaping at screen, display cat with stank attitude, nose turned up, claws out, sticking tongue out, 
 
-// user picks and enters a count
+    }
+    // create a reset game button. 
+
 
 };
