2d55665d812aeb0d989bea64197ff1edc4fe0531
Leia Quesada <leiaquesada@gmail.com>
2021-02-12 00:08:10 -0800

    Add JS DOM selectors

--- Games/CoinFlip/static/coinflip.js
+++ Games/CoinFlip/static/coinflip.js
@@ -7,25 +7,13 @@ Extensions:
 Add a box where the user can say how many coins they want to flip. Then display that many coins and flip them all randomly.
 */
 
-let coinFlips = document.getElementById("flipCount").value;
+const coinFlips = document.getElementById("flipCount");
+const image = document.getElementsByTagName("a");
+const userResultAlert = document.getElementById("userAlert");
+const display = document.getElementsByClassName("display");
 
-// default behavior is clearing the submission 
-function(e) {
-    if (e.preventDefault) e.preventDefault();
-    return false;
-}
 
-let form = document.getElementById("game-form");
-if (form.attachEvent) {
-    form.attachEvent("submit", processForm);
-} else {
-    form.addEventListener("submit", processForm);
-}
-
-// user picks and enters a number
-
-// store the number as coinFlips
-
+document.getElementById("game-form").onsubmit = () => {
 // display heads or tails consecutively after each call to flip, add coins raining in the background later
 
 // if heads results are greater than tails, congratulate player
@@ -36,5 +24,10 @@ if (form.attachEvent) {
 
 //play video of cat with claws out leaping at screen, display cat with stank attitude, nose turned up, claws out, sticking tongue out, 
 
+// if heads result === tails result return flip again
 
 // create a reset game button. 
+
+// user picks and enters a count
+
+};
