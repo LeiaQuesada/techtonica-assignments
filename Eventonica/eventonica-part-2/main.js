7625fab7ea712281a66c0d1ba33beac1b5628fc5
Leia Quesada <leiaquesada@gmail.com>
2021-02-19 23:01:10 -0800

    Remove event successfully

--- Eventonica/eventonica-part-2/main.js
+++ Eventonica/eventonica-part-2/main.js
@@ -14,34 +14,42 @@ document.addEventListener("DOMContentLoaded", () => {
       .join("\n");
   };
 
+  // Handle add event form submit
   const addEventForm = document.querySelector("#add-event");
-
-  // Handle add event form submit by calling our instance of Eventonica, `app`
   addEventForm.addEventListener("submit", (submitEvent) => {
-    submitEvent.preventDefault();
+    submitEvent.preventDefault();   // still don't understand what this does
     //capture input values and store in appropriate variables
     const name = document.querySelector("#add-event-name").value;
     const date = document.querySelector("#add-event-date").value;
     const category = document.querySelector("#add-event-category").value;
     // store event properties in event object
     const event = app.addEvent(name, date, category);
-
     // console.log("Added event", event);
     refreshEventsList();
     addEventForm.reset();
   });
 
-  // Builds HTML list for all users. call this function after you
-  // change, add, or remove any users?
+  // delete event
+  const deleteEventForm = document.querySelector("#delete-event");
+  deleteEventForm.addEventListener("submit", (deleteEvent) => {
+    deleteEvent.preventDefault(); 
+    // need to turn value into integer not string
+    const eventToBeRemoved = parseInt(document.querySelector("#delete-event-id").value);
+    const removeEvent = app.deleteEvent(eventToBeRemoved);
+    console.log("Did this delete yet? id: ", eventToBeRemoved, removeEvent);
+    refreshEventsList();
+    deleteEventForm.reset();
+  });
+
+  // Builds HTML list for all users
   const refreshUsersList = () => {
     document.querySelector("#users-list").innerHTML = User.all
       .map((user) => `<li>${user.userName}</li>`)
       .join("\n");
   };
 
+  // Handle add user form submit
   const addUserForm = document.querySelector("#add-user");
-
-  // Handle add user form submit by calling our instance of Eventonica, `app`
   addUserForm.addEventListener("submit", (submitUser) => {
     submitUser.preventDefault();
     const firstName = document.querySelector("#add-user-first-name").value;
