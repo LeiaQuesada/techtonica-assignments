ce93b5f98a87ec24022797c1dada90b58ea19331
Leia Quesada <leiaquesada@gmail.com>
2021-03-01 16:16:22 -0800

    set up node server

--- techtonica-labs/getting-started-with-node/app.js
+++ techtonica-labs/getting-started-with-node/app.js
@@ -1,10 +1,18 @@
+// creates a basic web server run with 'node app.js'
+// imports http module
 const http = require('http');
 
+// binds local server's web address
 const hostname = '127.0.0.1';
+//binds the port number our server is listening for requests and responses on
 const port = 3000;
 
+// creates a server instance
 const server = http.createServer((req, res) => {
+    // response status code set to ok
     res.statusCode = 200;
+    // response's header to plain text
     res.setHeader = ('Content-type', 'text/plain');
+    // concludes server's response after rendering message
     res.end('Hello my Peoples!');
 });
