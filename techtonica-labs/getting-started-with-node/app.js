e41bb7130196d7e5e57c3200e71089290473ccf2
Leia Quesada <leiaquesada@gmail.com>
2021-03-02 19:34:01 -0800

    Add write HTML instead of printing to console or node server res

--- techtonica-labs/getting-started-with-node/app.js
+++ techtonica-labs/getting-started-with-node/app.js
@@ -1,23 +1,33 @@
 // creates a basic web server run with 'node app.js'
 // imports http module
 const http = require('http');
+const fs = require('fs');
 
 // binds local server's web address
 const hostname = '127.0.0.1';
 //binds the port number our server is listening for requests and responses on
 const port = 3000;
 
-// creates a server instance
-const server = http.createServer((req, res) => {
+
+fs.readFile('index.html', (err, html) => {
+    if(err){
+        throw err;
+    }
+
+    // creates a server instance
+    const server = http.createServer((req, res) => {
     // response status code set to ok
-    res.statusCode = 200;
-    // response's header to plain text
-    res.setHeader = ('Content-type', 'text/plain');
-    // concludes server's response after rendering message
-    res.end('Hello my Peoples!');
+        res.statusCode = 200;
+        // response's header to plain text
+        res.setHeader = ('Content-type', 'text/html');
+        res.write(html);
+        // concludes server's response
+        res.end();
+    });
+
+// listen takes in port, hostname and callback, outputs text to console
+    server.listen(port, hostname, () => {
+        console.log('Server running on port ' + port + '...');
+    })
 });
 
-// listen 
-server.listen(port, hostname, () => {
-    console.log('Server running on port ' + port + '...');
-})
