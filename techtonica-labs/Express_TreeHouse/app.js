cc794e03ad1f4fd42032330dac395df6f0acc28b
Leia Quesada <leiaquesada@gmail.com>
2021-03-03 23:56:47 -0800

    Add arr loop that displays each element as a new list item

--- techtonica-labs/Express_TreeHouse/app.js
+++ techtonica-labs/Express_TreeHouse/app.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 
+const colors = [
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'purple'
+];
+
 // template engine enables us to use static template files in our app. At runtime, the template engine replaces variables in a template file with actual values and transforms the template into an HTML file sent to the client.
 //sets the view engine to the parameter pug 
 // npm install pug --save
@@ -11,8 +20,11 @@ app.get('/', (req, res) => {
     res.render('index.pug');
 });
 
-app.get('/hello' (req, res) => {
-    res.send('<h1>Oh lookie, another page..</h1>');
+app.get('/cards', (req, res) => {
+    // render takes two params, locals which can be defined like this:
+    // res.locals.prompt = "Who is buried in Grant's tomb?";
+    // or like this:
+    res.render('card', { prompt: "Who is buried in Grant's tomb?", colors });
 });
 
 app.listen(3000, () +> {
