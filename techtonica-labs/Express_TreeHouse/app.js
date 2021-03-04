6a2a344f4cc3291cb4e01476b6c73abc3fa5a0bb
Leia Quesada <leiaquesada@gmail.com>
2021-03-03 21:43:01 -0800

    add express server and pug template package

---
+++ techtonica-labs/Express_TreeHouse/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+//sets the view engine to the parameter pug 
+// npm install pug --save
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+    res.send('<h1>Let\'s see what this does..</h1>');
+});
+
+app.get('/hello' (req, res) => {
+    res.send('<h1>Oh lookie, another page..</h1>');
+});
+
+app.listen(3000, () +> {
+    console.log('This application is running on localhost:3000')
+})
+// template engine enables us to use static template files in our app. At runtime, the template engine replaces variables in a template file with actual values and transforms the template into an HTML file sent to the client.
+
