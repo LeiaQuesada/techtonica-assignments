8face7bdec594fb01c19b94f0d4fb3a0f35b525e
Leia Quesada <leiaquesada@gmail.com>
2021-03-04 00:50:41 -0800

    create an HTML form, route and post request

--- techtonica-labs/Express_TreeHouse/app.js
+++ techtonica-labs/Express_TreeHouse/app.js
@@ -1,22 +1,24 @@
 const express = require('express');
 const app = express();
 
-
-// template engine enables us to use static template files in our app. At runtime, the template engine replaces variables in a template file with actual values and transforms the template into an HTML file sent to the client.
-//sets the view engine to the parameter pug 
 // npm install pug --save
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
     // use render to use pug template
-    res.render('index.pug');
+    res.render('index');
 });
 
 app.get('/cards', (req, res) => {
-    // render takes two params, locals which can be defined like this:
-    // res.locals.prompt = "Who is buried in Grant's tomb?";
-    // or like this:
-    res.render('card', { prompt: "Who is buried in Grant's tomb?" });
+    res.render('cards', { prompt: "Who is buried in Grant's tomb?" });
+});
+
+app.get('/hello', (res, req) => {
+    res.render('hello');
+});
+
+app.post('/hello', (res, req) => {
+    res.render('hello');
 });
 
 app.listen(3000, () +> {
