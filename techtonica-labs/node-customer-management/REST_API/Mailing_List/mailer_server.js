5ec35581efeb169d6864c869f8ef693b2046a424
Leia Quesada <leiaquesada@gmail.com>
2021-03-03 18:04:55 -0800

    Add REST API functionality to toggle request methods

--- techtonica-labs/node-customer-management/REST_API/Mailing_List/mailer_server.js
+++ techtonica-labs/node-customer-management/REST_API/Mailing_List/mailer_server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 4000;
 
-// const app = express();
+const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -12,31 +12,71 @@ app.listen(port, () => {
   console.log(`Server running on port ${port}...`);
 });
 
-//creates a list object
-// let lists = new Map();
-
-app.route('/lists').get((req, res) => {
-  // creates existing list names
-  const listsArray = Array.from(lists.keys()); 
-  res.status(200).send(listsArray);
+app.get('/lists', (req, res) => {
+  const listsArray = Array.from(lists.keys());
+  res.send(listsArray);
 });
 
-// add array objects to lists object
-const lists = [
-  { 
-    "name": "staff",
-    "members": 
-      [
-        "talea@techtonica.org", 
-        "michelle@techtonica.org"
-      ]
-  },
-  {
-    "name1": "volunteers",
-    "members1": 
-      [
-        "me@techtonica.org", 
-        "you@techtonica.org"
-      ]
-  }
-]
+app.get('/lists/:name', (req, res) => {
+  let list_name = req.params.name;
+  let status = 400;
+  let response = "Unable to fetch data!";
+  lists.forEach((list) => {
+      if (list["name"] == list_name) {
+          res.status(200).send(list);
+      }
+  });
+  res.status(status).send(response);
+});
+
+app.delete('/lists/:name', (req, res) => {
+  let list_name = req.params.name;
+  let status = 400;
+  let response = "Unable to fetch data!";
+  let newLists = lists.filter((list) => {
+      return list;
+  });
+
+  status = 200;
+  response = newLists;
+  res.status(status).send(response);
+});
+
+app.put('/lists/:name', (req, res) => {
+  let list_name = req.params.name;
+  let status = 400;
+  let response = "Unable to fetch data!";
+  let newList = {};
+  lists.forEach((list) => {
+      newList = req.body;
+  });
+
+  status = 200;
+  response = newList;
+  res.status(status).send(response);
+});
+
+
+// new instance of Map object to allow us to retrieve object name, keys and values
+const lists = new Map();
+
+let staffList = { 
+  "name": "staff", 
+  "members": 
+    [
+      "talea@techtonica.org", 
+      "michelle@techtonica.org"
+    ]
+};
+
+var apprenticeList = {
+  name: "apprentices",
+  members: 
+    [
+      "me@gmail.com", 
+      "you@gmail.com"
+    ]
+};
+
+lists.set(`${staffList.name}`, staffList);
+lists.set(`${apprenticeList.name}`, apprenticeList);
