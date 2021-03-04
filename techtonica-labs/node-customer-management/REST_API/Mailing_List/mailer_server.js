0f7c6bdf63a3d57b60903c2923dac5cb87a89024
Leia Quesada <leiaquesada@gmail.com>
2021-03-03 16:49:25 -0800

    Add data to respond with on post request

--- techtonica-labs/node-customer-management/REST_API/Mailing_List/mailer_server.js
+++ techtonica-labs/node-customer-management/REST_API/Mailing_List/mailer_server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 // const app = express();
 app.use(bodyParser.json());
@@ -11,3 +11,32 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(port, () => {
   console.log(`Server running on port ${port}...`);
 });
+
+//creates a list object
+// let lists = new Map();
+
+app.route('/lists').get((req, res) => {
+  // creates existing list names
+  const listsArray = Array.from(lists.keys()); 
+  res.status(200).send(listsArray);
+});
+
+// add array objects to lists object
+const lists = [
+  { 
+    "name": "staff",
+    "members": 
+      [
+        "talea@techtonica.org", 
+        "michelle@techtonica.org"
+      ]
+  },
+  {
+    "name1": "volunteers",
+    "members1": 
+      [
+        "me@techtonica.org", 
+        "you@techtonica.org"
+      ]
+  }
+]
