111aba5a22b90270f0a1df62668fbbe42f03839a
Leia Quesada <leiaquesada@gmail.com>
2021-03-12 00:46:05 -0800

    Add PostgreSQL, client and routes for testing

--- todo/test/index.js
+++ todo/test/index.js
@@ -1,20 +1,110 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var app = express();
-app.use(bodyParser.urlencoded());
-app.use(bodyParser.json());
-var router = express.Router();
+// Load the Node Postgresql Library so we can interact with our
+// PostgreSQL Database
+const pg = require('pg')
 
-router.get('/',function(req,res){
-  res.json({"error" : false, "message" : "Hello !"});
-});
+// ---- Setup database connection
+const dbHost = 'echo.db.elephantsql.com:5432'
+const dbUsername = 'ediwaecd'
+const dbPassword = '8U-HcqbYWc_ASy2cKhRzB-_U7sIr6Tg2'
+// for the free tier on elephantsql the db name and username are the same
+const dbName = dbUsername
 
-router.post('/add',function(req,res){
-  res.json({"error" : false, "message" : "success", "data" : req.body.num1 + req.body.num2});
-});
+const dbConnString = `postgres://${dbUsername}:${dbPassword}@${dbHost}/${dbName}`
 
-app.use('/',router);
+// Create a global pool of postgresql connections
+const dbPool = new pg.Pool({ connectionString: dbConnString })
 
-app.listen(3000,function(){
-  console.log("I am listening at PORT 3000");
+// Register a callback function to log errors and
+// terminate the web server process if postgresql 
+// has a problem
+dbPool.on('error', (err, client) => {
+  console.error('Unexpected error on client', err)
+  process.exit(-1)
 })
+
+// Retrieve all todos as database records from postgresql and
+// provide them to the passed in callback function
+function getTodos(callbackFn) {
+  return dbPool.query('SELECT id, entry FROM todo_items', callbackFn)
+}
+
+// Save a todo to the postgresql database, and provide the
+// resulting database record to a callback function
+function saveTodo(todo, callbackFn) {
+  return dbPool.query(
+    'INSERT INTO todo_items (entry) VALUES($1)',
+    [todo],
+    callbackFn)
+}
+
+// Load the express library so we can make our
+// app available via the Internet
+const express = require('express')
+// Initialize an express application
+const app = express()
+
+// Use the body-parser express middleware so we can
+// use data encoded as JSON and POSTed to the server
+// in a request body
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
+
+
+// Register a route for GET requests to ‘/‘ 
+// that loads all the todos and shows 
+// them to the user.
+app.get('/items', (req, res) => {
+  getTodos((err, todoResult) => {
+    if (err) {
+      res.status(503).send('<b>Error getting TODO list</b>')
+      return
+    }
+
+    let items = ''
+    todoResult.rows.forEach(row => items += `<li>${row.entry}</li>`)
+    res.send(`<b>TODO list:</b><br/><ul>${items}</ul>`)
+  })
+})
+
+
+// Register a route to handle POST requests to ‘/‘
+// It creates a TODO in the database and 
+// redirects users
+app.post('/', (req, res) => {
+  if (!req.body || !req.body.todo) {
+    res.status(400).send('Expected TODO item')
+    return
+  }
+
+  saveTodo(req.body.todo, (err, dbRes) => {
+    if (err) {
+      res.status(503).send(`Unable to save new TODO item: ${req.body.todo}`)
+      return
+    }
+    res.redirect('/')
+  })
+})
+
+// Register a route to handle GET requests to ’/’
+// that loads TODO’s from the database and exposes them 
+// to the user as JSON
+app.get('/', (req, res) => {
+  getTodos((err, todoResult) => {
+    const hasError = !!err
+    const result = {
+      error: hasError,
+      // if we got a DB error return no data to avoid inconsistent results
+      todo: hasError ? [] : todoResult.rows,
+    }
+
+    const respCode = hasError ? 503 : 200
+    res.status(respCode).send(JSON.stringify(result))
+  })
+})
+
+// Configure our application to listen to requests on port 3000.
+// Register a callback so that once the server starts it logs that
+// it’s ready and listening.
+const port = 3000;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- todo/test/test.js
+++ todo/test/test.js
@@ -2,17 +2,13 @@ var supertest = require("supertest");
 var should = require("should");
 
 // This agent refers to PORT where program is runninng.
-
 var server = supertest.agent("http://localhost:3000");
 
 // UNIT test begin
-
 describe("SAMPLE unit test",function(){
 
   // #1 should return home page
-
   it("should return home page",function(done){
-
     // calling home page api
     server
     .get("/")
@@ -28,7 +24,6 @@ describe("SAMPLE unit test",function(){
   });
 
   it("should add two number",function(done){
-
     //calling ADD api
     server
     .post('/add')
